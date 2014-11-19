d08ae359327f34fdba9051ab870dc6ad8bbe7a92
Peter <peterhsaxton@gmail.com>
2014-11-19 22:56:43 +0000

    fetch ticket price

--- lib/party.rb
+++ lib/party.rb
@@ -6,7 +6,29 @@ class Party < Hash
   end
 
   def ticket_price=(amount)
-    self[:ticket_fractional] = amount.fractional
-    self[:ticket_currency] = amount.currency
+    self.ticket_fractional = amount.fractional
+    self.ticket_currency = amount.currency
+  end
+
+  def ticket_price
+    Money.new(ticket_fractional, ticket_currency)
+  end
+
+  private
+
+  def ticket_fractional=(fractional)
+    self[:ticket_fractional] = fractional
+  end
+
+  def ticket_currency=(currency)
+    self[:ticket_currency] = currency
+  end
+
+  def ticket_fractional
+    fetch :ticket_fractional
+  end
+
+  def ticket_currency
+    fetch :ticket_currency
   end
 end
--- test/party/party_test.rb
+++ test/party/party_test.rb
@@ -5,4 +5,9 @@ class PartyTest < UnitTest
     party = Party.new ticket_price: Money.new(1200, 'bhd')
     assert_equal({ticket_fractional: 1200, ticket_currency: 'bhd'}, party)
   end
+
+  def test_party_fetches_ticket_price
+    party = Party.new ticket_fractional: 1200, ticket_currency: 'bhd'
+    assert_equal Money.new(1200, 'bhd'), party.ticket_price
+  end
 end
