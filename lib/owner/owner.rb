25370884f4232844cc6e29ebe625f61e9978ff13
Peter <peterhsaxton@gmail.com>
2014-11-23 19:46:08 +0000

    save owner in record

--- lib/owner/owner.rb
+++ lib/owner/owner.rb
@@ -1,56 +1,51 @@
-class Owner < Hash
-  def initialize(args={})
-    args.each{ |attribute, value|
-      send("#{attribute}=", value)
-    }
-  end
+class Owner < Thyng
+  value_accessor :name
+  value_accessor :guid
+end
 
-  def name=(name)
-    self[:name] = name
-  end
+class Owner
+  class Record < Sequel::Model(:owners)
+    def initialize(*args, &block)
+      super
+      self.guid = SecureRandom.uuid()
+    end
 
-  def name
-    fetch(:name)
-  end
-
-  def work_contact_point=(work_contact_point)
-    self[:work_contact_point] = work_contact_point
-  end
-
-  def work_contact_point
-    fetch(:work_contact_point)
+    def self.save owner
+      record = new.set(owner).save
+      owner.guid = record.guid
+    end
   end
 end
 
-class Account < Sequel::Model(:accounts)
-  def initialize(*args, &block)
-    super
-    self.guid = SecureRandom.uuid()
-  end
+# class Account < Sequel::Model(:accounts)
+  # def initialize(*args, &block)
+  #   super
+  #   self.guid = SecureRandom.uuid()
+  # end
 
-  def owner=(owner)
-    set owner
-  end
+#   def owner=(owner)
+#     set owner
+#   end
 
-  def work_contact_point=(contact_point)
-    nu_hash = Hash[contact_point.map {|k, v| ["work_#{k}", v] }]
-    set nu_hash
-  end
+#   def work_contact_point=(contact_point)
+#     nu_hash = Hash[contact_point.map {|k, v| ["work_#{k}", v] }]
+#     set nu_hash
+#   end
 
-  def owner
-    Owner.new(name: name, work_contact_point: work_contact_point)
-  end
+#   def owner
+#     Owner.new(name: name, work_contact_point: work_contact_point)
+#   end
 
-  def work_contact_point
-    ContactPoint.new(email: work_email, telephone: work_telephone)
-  end
+#   def work_contact_point
+#     ContactPoint.new(email: work_email, telephone: work_telephone)
+#   end
 
-  def self.get_all_work_contacts
-    all.map(&:work_contact_point)
-  end
+#   def self.get_all_work_contacts
+#     all.map(&:work_contact_point)
+#   end
 
-  def self.last_owner
-    last.owner
-  end
+#   def self.last_owner
+#     last.owner
+#   end
 
-end
+# end
--- test/owner/owner_test.rb
+++ test/owner/owner_test.rb
@@ -1,9 +1,9 @@
 require_relative '../test_config'
 
 class OwnerTest < UnitTest
-  def empty_account
-    Account.create
-  end
+  # def empty_account
+  #   Account.create
+  # end
 
   def telephone
     '+44 1234 567'
@@ -21,28 +21,34 @@ class OwnerTest < UnitTest
     Owner.new name: 'steve'
   end
 
-  def test_can_update_work_contact_point
-    empty_account.update work_contact_point: contact_point
-    assert_equal({work_email: email, work_telephone: telephone}, Account.last.values.select{|k| k == :work_telephone || k == :work_email})
+  def test_can_save_name
+    owner = Owner.new name: 'kevin'
+    Owner::Record.save owner
+    assert_equal 'kevin', Owner::Record.last.name
+    assert owner.guid, 'Owner should have id'
   end
 
   def test_can_get_all_work_contacts
+    skip
     Account.create
     assert_equal ContactPoint, Account.get_all_work_contacts.first.class
   end
 
   def test_can_update_owner
+    skip
     empty_account.update owner: owner
     assert_equal owner.name, Account.last.name
   end
 
   def test_can_update_owner_with_work_contact_point
+    skip
     o = owner.merge(work_contact_point: contact_point)
     empty_account.update owner: o
     assert_equal telephone, Account.last.work_telephone
   end
 
   def test_get_compound_contact_point
+    skip
     o = owner.merge(work_contact_point: contact_point)
     empty_account.update owner: o
     assert_equal contact_point, Account.last_owner.work_contact_point
