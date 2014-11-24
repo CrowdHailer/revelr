ab96ab34268792f37d217d3dbe4d01a20b2edfb6
Peter <peterhsaxton@gmail.com>
2014-11-24 15:29:29 +0000

    party controller

---
+++ app/controllers/parties.rb
@@ -0,0 +1,17 @@
+Revelr::App.controllers :parties do
+  
+  get :index do
+    # @parties = Party.all
+    render :'parties/index'
+  end
+
+  get :new, :map => '/parties/new' do
+    render:'parties/new'
+  end
+
+  post :create, :map => '/parties' do
+    puts params
+    redirect to('/parties')
+  end
+
+end
